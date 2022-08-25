1e8ea4f42e65c2db2307b8565fdc8e5ea4eeed32
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-24 22:39:34 -0700

    create resume component and add content

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,18 +13,21 @@ const styles = {
 }
 const Portfolio = () => {
   return (
-    <Card style={styles.width}>
-      <Card.Img variant="top" src={Narratorium} />
-      <Card.Body>
-        <Card.Title>Narratorium</Card.Title>
-        <Card.Text>
-          <span style={styles.bold} >Front-end: </span>HTML, CSS, Javascript, Fetch, Web API, Session Storage, Skeleton, Figma, Quill JS
-          <br/><br/>
-          <span style={styles.bold}>Back-end: </span>Node, NPM, Express, MySQL, Sequelize, User Authentication via bcrypt, Handlebars, Socket.io
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div>
+      <h2>Portfolio</h2>
+      <Card style={styles.width}>
+        <Card.Img variant="top" src={Narratorium} />
+        <Card.Body>
+          <Card.Title>Narratorium</Card.Title>
+          <Card.Text>
+            <span style={styles.bold} >Front-end: </span>HTML, CSS, Javascript, Fetch, Web API, Session Storage, Skeleton, Figma, Quill JS
+            <br /><br />
+            <span style={styles.bold}>Back-end: </span>Node, NPM, Express, MySQL, Sequelize, User Authentication via bcrypt, Handlebars, Socket.io
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
