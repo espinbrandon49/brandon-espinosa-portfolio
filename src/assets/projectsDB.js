bbcd101884e46070756bcb755473cb96e17137d1
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-25 21:27:34 -0700

    populate propjects db

--- src/assets/projectsDB.js
+++ src/assets/projectsDB.js
@@ -1,62 +1,56 @@
 module.exports = [
   {
-    name: "Project 1",
-    technologies: "JavaScript",
-    deployedLink: "https://www.google.com/",
-    githubRepo: "https://github.com/espinbrandon49/brandon-espinosa-portfolio",
-    img: "narratorium",
-    imgAlt: "project 1 img alt",
-    description: "really fun description",
-    tools: "tool1, tool2, tool3"
+    name: "Server-Side API",
+    technologies: "JavaScript, HTML, CSS, Git, GitHub, Chrome DevTools",
+    deployedLink: "https://espinbrandon49.github.io/easy-access-weather/",
+    githubRepo: "https://github.com/espinbrandon49/easy-access-weather",
+    img: "easy-access-weather",
+    imgAlt: "A weather dashboard to see the weather outlook for multiple cities.",
+    description: "A weather dashboard to allow a user to see the weather outlook for multiple cities."
   },
   {
-    name: "Project 1",
-    technologies: "JavaScript",
-    deployedLink: "https://www.google.com/",
-    githubRepo: "https://github.com/espinbrandon49/brandon-espinosa-portfolio",
-    img: "narratorium",
-    imgAlt: "project 1 img alt",
-    description: "really fun description",
-    tools: "tool1, tool2, tool3"
+    name: "Interactive Front-End",
+    technologies: "JavaScript, HTML, CSS, Skeleton (style), server-side API, client-side storage (local storage), leaflet (map)",
+    deployedLink: "https://espinbrandon49.github.io/EV-Charging-Station-Locator/",
+    githubRepo: "https://github.com/espinbrandon49/EV-Charging-Station-Locator",
+    img: "ev-charging-locator",
+    imgAlt: "App that collects data based on a person's location search and displays map and retail information.",
+    description: "App that collects data based on a person's location search and displays map and retail information."
   },
   {
-    name: "Project 1",
-    technologies: "JavaScript",
-    deployedLink: "https://www.google.com/",
-    githubRepo: "https://github.com/espinbrandon49/brandon-espinosa-portfolio",
+    name: "Interactive Full-Stack",
+    technologies: "FRONT-END: HTML, CSS, Javascript, Fetch, Web API, Session Storage, Skeleton, Figma, Quill.js \n BACK-END: Node, NPM, Express, MySQL, Sequelize, user authentication, Handlebars, Socket.io",
+    deployedLink: "https://narratorium.herokuapp.com/",
+    githubRepo: "https://github.com/espinbrandon49/narratorium",
     img: "narratorium",
-    imgAlt: "project 1 img alt",
-    description: "really fun description",
-    tools: "tool1, tool2, tool3"
+    imgAlt: "A collabrative story-telling website designed to allow anyone on the site to contribute to a single story.",
+    description: "A collabrative story-telling website designed to allow anyone on the site to contribute to a single story."
   },
   {
-    name: "Project 1",
-    technologies: "JavaScript",
-    deployedLink: "https://www.google.com/",
-    githubRepo: "https://github.com/espinbrandon49/brandon-espinosa-portfolio",
-    img: "narratorium",
-    imgAlt: "project 1 img alt",
-    description: "really fun description",
-    tools: "tool1, tool2, tool3"
+    name: "Model-View-Controller",
+    technologies: "JavaScript, Handlebars, Node.js, Bootstrap, Express, MySQL2, Sequalize, express-session, connect-session-sequelize, bcrypt",
+    deployedLink: "https://hackathought.herokuapp.com/",
+    githubRepo: "https://github.com/espinbrandon49/hack-a-thought",
+    img: "hack-a-thought",
+    imgAlt: "CMS-style blog site, where developers can publish their blog posts and comment on posts.",
+    description: "CMS-style blog site, where developers can publish their blog posts and comment on posts."
   },
   {
-    name: "Project 1",
-    technologies: "JavaScript",
-    deployedLink: "https://www.google.com/",
-    githubRepo: "https://github.com/espinbrandon49/brandon-espinosa-portfolio",
-    img: "narratorium",
-    imgAlt: "project 1 img alt",
-    description: "really fun description",
-    tools: "tool1, tool2, tool3"
+    name: "Web APIs",
+    technologies: "HTML, CSS, JavaScript, Web API, local stroage",
+    deployedLink: "https://espinbrandon49.github.io/javaScriptQuiz/",
+    githubRepo: "https://github.com/espinbrandon49/javaScriptQuiz",
+    img: "codeQuiz",
+    imgAlt: "Timed coding quiz with multiple-choice questions features dynamically updated HTML and CSS powered by JavaScript.",
+    description: "Timed coding quiz with multiple-choice questions features dynamically updated HTML and CSS powered by JavaScript.",
   },
   {
-    name: "Project 1",
-    technologies: "JavaScript",
-    deployedLink: "https://www.google.com/",
-    githubRepo: "https://github.com/espinbrandon49/brandon-espinosa-portfolio",
-    img: "narratorium",
-    imgAlt: "project 1 img alt",
-    description: "really fun description",
-    tools: "tool1, tool2, tool3"
+    name: "Express.js",
+    technologies: "JavaScript, HTML, CSS, Express.js, Bootstrap, Node.js, Heroku, Git, GitHub",
+    deployedLink: "https://stark-taiga-57338.herokuapp.com/",
+    githubRepo: "https://github.com/espinbrandon49/stark_bare_notes",
+    img: "stark-bare-notes",
+    imgAlt: "Note Taker that can be used to write and save notes, uses an Express.js back end.",
+    description: "Note Taker that can be used to write and save notes, uses an Express.js back end.",
   }
 ]
